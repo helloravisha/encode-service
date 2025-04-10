95977d6592c546dd949a40dbd78771253088d8c2
Ravi Shanker katta <helloravisha@gmail.com>
2025-04-10 16:54:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, Order Service !"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
